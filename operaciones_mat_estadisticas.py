108371f2884fad0cfbdec76d393b9cc8ffc48d5b
Ferry <lopezdelcidferrydejesus@gmail.com>
2025-07-24 21:38:28 -0600

    Agregué las funciones para la opcion dos, la de area y la de perimetro separadas y la funcion donde llamo dichas funciones para el calculo

--- operaciones_mat_estadisticas.py
+++ operaciones_mat_estadisticas.py
@@ -36,3 +36,25 @@ def show_statistics():
     print(f"Ceros: {zeros}")
     print(f"Multiplos de 3: {multiples_3}")
 
+def rectangle_area(base, height):
+    return base * height
+
+def rectangle_perimeter(base, height):
+    return 2 * (base + height)
+
+def rectangle_operations():
+    while True:
+        try:
+            base = float(input("Ingrese la base del rectángulo: "))
+            height = float(input("Ingrese la altura del rectángulo: "))
+            if base <= 0 or height <= 0:
+                print("Los numeros deben ser positivos.")
+                continue
+            break
+        except ValueError:
+            print('Debes ingresar numeros validos.')
+    area = rectangle_area(base, height)
+    perimeter = rectangle_perimeter(base, height)
+    print(f'El area del rectangulo es: {area:.2f}')
+    print(f"El perimetro del rectangulo es: {perimeter:.2f}")
+
