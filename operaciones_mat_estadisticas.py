b92bba822366c7316966d8c7a14a47fde4a05efc
Ferry <lopezdelcidferrydejesus@gmail.com>
2025-07-24 21:26:39 -0600

    Agregué la función de mostrar las estadisticas de n numeros, suma, promedio, negativo, positivo, ceros y multiplos de 3

--- operaciones_mat_estadisticas.py
+++ operaciones_mat_estadisticas.py
@@ -18,3 +18,21 @@ def enter_numbers(message):
             except ValueError:
                 print("Debe ingresar un número válido")
     return numbers
+
+def show_statistics():
+    numbers = enter_numbers("Ingrese cuantos números desea: ")
+    if not numbers
+        return
+    add = sum(numbers)
+    average = add / len(numbers)
+    positives = sum(1 for num in numbers if num > 0)
+    negatives = sum(1 for num in numbers if num < 0)
+    zeros = sum(1 for num in numbers if num == 0)
+    multiples_3 = sum(1 for num in numbers if num % 3 == 0)
+    print(f"Suma total: {add:.2f}")
+    print(f"Promedio: {average:.2f}")
+    print(f"Positivos: {positives}")
+    print(f"Negativos: {negatives}")
+    print(f"Ceros: {zeros}")
+    print(f"Multiplos de 3: {multiples_3}")
+
